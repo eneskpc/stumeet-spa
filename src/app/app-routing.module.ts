f468816b5e758a38916a54c38265c50dbece0824
Enes Kapucu <enes.kapucu@outlook.com.tr>
2019-04-21 03:14:39 +0300

    tasarım giydirildi fakat sayfalar hazırlanacak

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { HomeComponent } from './home/home.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [{
+      path: '',
+      component: WelcomeComponent
+    }, {
+      path: 'giris',
+      component: LoginComponent
+    }]
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{
+      path: 'haber-kaynagi',
+      component: HomeComponent
+    }, {
+      path: 'etkinlikler',
+      component: EventsComponent
+    }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      MainLayoutComponent,
+      PublicLayoutComponent,
+      LoginComponent,
+      EventsComponent,
+      WelcomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
